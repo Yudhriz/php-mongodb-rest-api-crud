f7a076162b7f65b8ef533cbf9bc239185d8015b3
Yudhriz <riskyyudha678@gmail.com>
2023-11-23 13:12:45 +0700

    php rest api mongo db

---
+++ rest_resource_create.php
@@ -0,0 +1,36 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: ");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// include database file 
+include_once 'mongodb_config.php';
+$dbname = 'toko';
+$collection = 'barang';
+
+//DB connection
+$db = new DbManager();
+$conn = $db->getConnection();
+
+//record to add
+$data = json_decode(file_get_contents("php://input", true));
+
+// insert record
+$insert = new MongoDB\Driver\BulkWrite();
+$insert->insert($data);
+
+$result = $conn->executeBulkwrite("$dbname.$collection", $insert);
+
+// verify
+if ($result->getInsertedCount() == 1) {
+    echo json_encode(
+        array("message" => "Record successfully created")
+    );
+} else {
+    echo json_encode(
+        array("message" => "Error while saving record")
+    );
+}
